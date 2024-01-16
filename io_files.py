a472bf788e691b919ffe2a5f7891d037cf4888f9
Ollaid01 <36774779+Ollaid01@users.noreply.github.com>
2024-01-16 19:04:11 +0000

    just try another example

--- io_files.py
+++ io_files.py
@@ -7,4 +7,10 @@ file_data = my_file.read()  # read data
 my_file.close()  # close file and free resource
 
 # print data
+# print(file_data)
+
+# read another file (.py)
+pyfile = open('./tuples.py', 'r')
+file_data = pyfile.read()
+pyfile.close()
 print(file_data)
