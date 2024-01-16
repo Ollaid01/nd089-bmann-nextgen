d8d186baf6aee14827b55df455529c38da0551a7
Ollaid01 <36774779+Ollaid01@users.noreply.github.com>
2024-01-16 19:24:00 +0000

    with keyword : no needs file.close()

--- io_files.py
+++ io_files.py
@@ -13,12 +13,17 @@ my_file.close()  # close file and free resource
 pyfile = open('./tuples.py', 'r')
 file_data = pyfile.read()
 pyfile.close()
-print(file_data)
+# print(file_data)
 
 
 # Writing Files
 pyfile = open('./tuples.py', 'w')
 pyfile.write("Wrrrrrite  Hello There !")
 pyfile.close()
+# print(file_data)
+
+# Using auto-close files
+with open('./tuples.py', 'r') as myFile:
+    file_data = myFile.read()
 print(file_data)
 
